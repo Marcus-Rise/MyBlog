86e4f131368e1f022d3a6af5bf85cb3093fee5c6
Ilya Konstantinov <ilykonst95@gmail.com>
2020-08-15 12:03:41 +0400

    ci

---
+++ app/src/server/bootstrap.ts
@@ -0,0 +1,41 @@
+import { NestExpressApplication } from "@nestjs/platform-express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import csurf from "csurf";
+import Next from "next";
+import { RenderModule } from "nest-next";
+import { resolve } from "path";
+
+export const bootstrap = async (server: NestExpressApplication): Promise<void> => {
+    const dev = process.env.NODE_ENV === "development";
+    const app = Next({
+        dev,
+        dir: dev ? resolve(__dirname, "../client") : resolve(__dirname, "../../dist/client"),
+    });
+    await app.prepare();
+    const renderer = server.get(RenderModule);
+    renderer.register(server, app, { viewsDir: "", dev });
+
+    server.use(cookieParser(process.env.COOKIE_SECRET || "COOKIE_SECRET"));
+    server.use(csurf({ cookie: true }));
+
+    switch (process.env.NODE_ENV) {
+        case "development":
+            server.use(morgan("dev"));
+            break;
+        case "test":
+            server.use(morgan("tiny"));
+            break;
+        case "production":
+            server.use(
+                morgan("combined", {
+                    skip: (req, res) => res.statusCode < 400,
+                }),
+            );
+            server.use(helmet());
+            server.use(cors());
+            break;
+    }
+};
--- app/src/server/main.ts
+++ app/src/server/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
+import { bootstrap } from "./bootstrap";
 
 (async () => {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
     const PORT = Number(process.env.PORT || 3000);
+
+    await bootstrap(app);
 
     await app.listen(PORT, async () => {
         console.log(`> Ready on http://localhost:${PORT}`);
