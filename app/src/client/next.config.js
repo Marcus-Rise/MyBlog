871a36dbfb3ec843302654d814b1d2028d9e0c17
Ilya Konstantinov <ilykonst95@gmail.com>
2020-08-25 11:46:17 +0400

    pwa config

--- app/src/client/next.config.js
+++ app/src/client/next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+/* eslint-disable */
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
     reactStrictMode: true,
     distDir: process.env.NODE_ENV === "production" ? "../../dist/client/.next" : undefined,
-};
+});
