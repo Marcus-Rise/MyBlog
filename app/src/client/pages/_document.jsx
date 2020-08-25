6bb2d943912680a27c314114bff6fbee6555f548
Ilya Konstantinov <ilykonst95@gmail.com>
2020-08-25 16:25:38 +0400

    fix

--- app/src/client/pages/_document.jsx
+++ app/src/client/pages/_document.jsx
@@ -13,18 +13,18 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="manifest" href="/manifest.json" />
                     <link
-                        href="/icons/favicon-16x16-dunplab-manifest-10525.png"
+                        href="/icon/favicon-16x16-dunplab-manifest-10525.png"
                         rel="icon"
                         type="image/png"
                         sizes="16x16"
                     />
                     <link
-                        href="/icons/favicon-32x32-dunplab-manifest-10525.png"
+                        href="/icon/favicon-32x32-dunplab-manifest-10525.png"
                         rel="icon"
                         type="image/png"
                         sizes="32x32"
                     />
-                    <link rel="apple-touch-icon" href="/icons/apple-icon-57x57-dunplab-manifest-10525.png" />
+                    <link rel="apple-touch-icon" href="/icon/apple-icon-57x57-dunplab-manifest-10525.png" />
                     <meta name="theme-color" content="#445768" />
                 </Head>
                 <body>
