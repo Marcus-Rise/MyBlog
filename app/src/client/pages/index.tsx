8f18edf1b5f924c2a76021d418733e3e6e3afbe6
Ilya Konstantinov <ilykonst95@gmail.com>
2020-08-26 16:45:44 +0400

    feat: seo

--- app/src/client/pages/index.tsx
+++ app/src/client/pages/index.tsx
@@ -2,18 +2,27 @@ import { IndexPageProps } from "../dto/IndexPageProps";
 import React from "react";
 import { NextPage, NextPageContext } from "next";
 import { PostCard } from "../components/PostCard";
+import { NextSeo } from "next-seo";
 
 const Index: NextPage<IndexPageProps> = (props) => {
     return (
-        <div className="container pt-5">
-            {props.posts.map((i) => (
-                <div className="row mb-3" key={i.id}>
-                    <div className="col-12">
-                        <PostCard title={i.title} slug={i.slug} createdAt={i.createdAt} description={i.description} />
+        <React.Fragment>
+            <NextSeo title={"MarcusBlog"} description={"Ilya Konstantinov web developer"} />
+            <div className="container pt-5">
+                {props.posts.map((i) => (
+                    <div className="row mb-3" key={i.id}>
+                        <div className="col-12">
+                            <PostCard
+                                title={i.title}
+                                slug={i.slug}
+                                createdAt={i.createdAt}
+                                description={i.description}
+                            />
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </React.Fragment>
     );
 };
 
--- app/src/client/pages/post.tsx
+++ app/src/client/pages/post.tsx
@@ -4,27 +4,31 @@ import { PostPageProps } from "../dto/PostPageProps";
 import { DateDisplay } from "../components/DateDisplay";
 import { RichText } from "prismic-reactjs";
 import { htmlSerializer } from "../components/htmlSerializer";
+import { NextSeo } from "next-seo";
 
 const Post: NextPage<PostPageProps> = ({ post }) => {
     return (
-        <div className="container pt-5">
-            <div className="row">
-                <div className="col-12">
-                    <div className="card border-0">
-                        <div>
-                            <h5 className="card-title">{post.title}</h5>
-                            <h6 className="card-subtitle mb-2 text-muted">
-                                <DateDisplay date={post.createdAt} />
-                            </h6>
-                        </div>
-                        <hr />
-                        <div>
-                            <RichText render={post.content} htmlSerializer={htmlSerializer} />
+        <React.Fragment>
+            <NextSeo title={`MarcusBlog | ${post.title}`} description={post.description} />
+            <div className="container pt-5">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="card border-0">
+                            <div>
+                                <h5 className="card-title">{post.title}</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">
+                                    <DateDisplay date={post.createdAt} />
+                                </h6>
+                            </div>
+                            <hr />
+                            <div>
+                                <RichText render={post.content} htmlSerializer={htmlSerializer} />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </React.Fragment>
     );
 };
 
