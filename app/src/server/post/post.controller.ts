ab189de66d76fec5d75b263bb21fdde39326f45b
Ilya Konstantinov <ilykonst95@gmail.com>
2020-08-26 16:30:15 +0400

    feat: add caching

--- app/src/server/post/post.controller.ts
+++ app/src/server/post/post.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, NotFoundException, Param, Render } from "@nestjs/common";
+import {
+    CacheInterceptor,
+    CacheTTL,
+    Controller,
+    Get,
+    NotFoundException,
+    Param,
+    Render,
+    UseInterceptors,
+} from "@nestjs/common";
 import { PostService } from "./post.service";
 import { POST_PAGE_URL, PostPageProps } from "../../client/dto/PostPageProps";
 import { PostListItemDto } from "./post-list-item.dto";
@@ -6,9 +15,11 @@ import { INDEX_PAGE_URL, IndexPageProps } from "../../client/dto/IndexPageProps"
 import { PostDto } from "./post.dto";
 
 @Controller()
+@UseInterceptors(CacheInterceptor)
 export class PostController {
     constructor(private readonly postService: PostService) {}
 
+    @CacheTTL(20)
     @Get(INDEX_PAGE_URL)
     @Render("index")
     async index(): Promise<IndexPageProps> {
@@ -17,6 +28,7 @@ export class PostController {
         return { posts: posts.map((i) => new PostListItemDto(i)) };
     }
 
+    @CacheTTL(20)
     @Get(POST_PAGE_URL + "/:slug")
     @Render("post")
     async getBySlug(@Param("slug") slug: string): Promise<PostPageProps> {
--- app/src/server/post/post.module.ts
+++ app/src/server/post/post.module.ts
@@ -1,10 +1,10 @@
-import { Logger, Module } from "@nestjs/common";
+import { CacheModule, Logger, Module } from "@nestjs/common";
 import { PostService } from "./post.service";
 import { PostController } from "./post.controller";
 import { PrismicModule } from "../prismic/prismic.module";
 
 @Module({
-    imports: [PrismicModule],
+    imports: [CacheModule.register(), PrismicModule],
     providers: [PostService, Logger],
     controllers: [PostController],
     exports: [PostService],
