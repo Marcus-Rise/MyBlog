7c51d9581865b6d92f3de41d430fe36c6a13cc66
Ilya Konstantinov <ilykonst95@gmail.com>
2020-08-27 13:22:00 +0400

    fix(post.tsx)

--- app/src/client/pages/post.tsx
+++ app/src/client/pages/post.tsx
@@ -6,19 +6,22 @@ import { htmlSerializer } from "../components/htmlSerializer";
 import { NextSeo } from "next-seo";
 import { PostHeader } from "../components/PostHeader";
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
 const Post: NextPage<PostPageProps> = ({ post }) => {
     return (
         <React.Fragment>
             <NextSeo title={`MarcusBlog | ${post.title}`} description={post.description} />
-            <Card className="pt-3 border-0">
-                <Card.Body>
-                    <PostHeader tags={post.tags} title={post.title} createdAt={post.createdAt} />
-                    <Card.Text>
-                        <RichText render={post.content} htmlSerializer={htmlSerializer} />
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <Container>
+                <Card className="pt-3 border-0">
+                    <Card.Body>
+                        <PostHeader tags={post.tags} title={post.title} createdAt={post.createdAt} />
+                        <Card.Text>
+                            <RichText render={post.content} htmlSerializer={htmlSerializer} />
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Container>
         </React.Fragment>
     );
 };
