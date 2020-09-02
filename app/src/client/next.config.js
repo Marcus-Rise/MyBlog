fa78acb97f3b87ea4465e72fcf1233282e8c14a5
Ilya Konstantinov <ilykonst95@gmail.com>
2020-09-02 09:50:03 +0400

    ci(next.config.js)

--- app/src/client/next.config.js
+++ app/src/client/next.config.js
@@ -3,6 +3,9 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
+    pwa: {
+        disable: process.env.NODE_ENV !== "production",
+    },
     reactStrictMode: true,
     distDir: process.env.NODE_ENV === "production" ? "../../dist/client/.next" : undefined,
     crossOrigin: "anonymous",
